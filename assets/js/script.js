75838d2e2f3b09b132a52a41bf17ac429b9509d7
bradleydeutsch <blades@tesco.net>
2013-12-01 13:41:20 +0000

    Tidy Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,34 @@ var global = {},
 	gallery;
 
 function Gallery(imagesArr) {
-	var gallery = {
-			CONSTANTS: {
-				SINGLE_WIDTH: 200,
-				MARGIN: 20,
-				BORDER: 1,
-				GAP_THRESHOLD: 150
-			},
+	var CONSTANTS = {
+			SINGLE_WIDTH: 200,
+			MARGIN: 20,
+			BORDER: 1,
+			GAP_THRESHOLD: 150
 		};
 
-	gallery.CALCULATED_IMAGE_WIDTH = gallery.CONSTANTS.SINGLE_WIDTH + (gallery.CONSTANTS.BORDER * 2);
-
+	this.calculatedImageWidth = CONSTANTS.SINGLE_WIDTH + (CONSTANTS.BORDER * 2);
 	this.imagesArr = imagesArr;
+	this.galleryContainer = $('div#gallery');
+	this.columnCount = 3;
 
-	this.setupGallery = function () {
+	this.init = function () {
+		var _this = this;
+
+		//_this.imagesArr = _this.randomizeImages(_this.imagesArr);
+		_this.determineColumns();
+		_this.setupGalleryContainer();
+		_this.arrangeGallery();
+	}
+
+	this.randomizeImages = function (imagesArr) {
+		for(var j, x, i = imagesArr.length; i; j = Math.floor(Math.random() * i), x = imagesArr[--i], imagesArr[i] = imagesArr[j], imagesArr[j] = x);
+		
+		return imagesArr;
+	}
+
+	this.determineColumns = function () {
 		var _this = this,
 			windowWidth = $(window).width(),
 			columns = 3;
@@ -27,79 +41,86 @@ function Gallery(imagesArr) {
 		} else if (windowWidth > 1000) {
 			columns = 4;
 		}
-		_this.buildGallery(columns);
+
+		_this.columnCount = columns;
 	}
 
-	this.buildGallery = function (columns) {
+	this.setupGalleryContainer = function () {
 		var _this = this,
-			galleryContainer = $('div#gallery'),
-			galleryWidth = (columns * gallery.CALCULATED_IMAGE_WIDTH) + (gallery.CONSTANTS.MARGIN * (columns - 1));
+			galleryWidth = (_this.columnCount * _this.calculatedImageWidth) + (CONSTANTS.MARGIN * (_this.columnCount - 1));
 
-		galleryContainer.width(galleryWidth);
-		_this.arrangeGallery(galleryContainer, columns);
+		_this.galleryContainer.width(galleryWidth);
 	}
 
-	this.arrangeGallery = function (galleryContainer, columns) {
+	this.createImage = function (image) {
+		var _this = this,
+			$image = $('div#image-' + image.id);
+
+		if ($image.length === 0) {
+			$image = $(_.template($('#image-template').html())(_.extend(image, {
+				src: 'http://placekitten.com/' + (((Math.floor(Math.random() * 3) + 1) % 3 === 0) ? 'g/' : '') + image.width + '/' + image.height + '/'
+			})));
+
+			$image.find('a').bind('click', function (evt) {
+				evt.preventDefault();
+
+				alert('Woof!');
+			});
+
+			_this.galleryContainer.append($image);
+		}
+
+		return $image;
+	}
+
+	this.arrangeGallery = function () {
 		var _this = this,
 			colHeights = [],
 			colCount = 0,
 			image,
 			$image,
-			single,
+			isSingle,
 			max,
 			i;
 
-		for (i = 0; i < columns; i++) {
+		for (i = 0; i < _this.columnCount; i++) {
 			colHeights[i] = 0;
 		}
 
 		for (i = 0; i < _this.imagesArr.length; i++) {
 			image = _this.imagesArr[i];
-			$image = $('div#image-' + image.id);
+			$image = _this.createImage(image);
 
-			if ($image.length === 0) {
-				$image = $(_.template($('#image-template').html())(_.extend(image, {
-					src: 'http://placekitten.com/' + (((Math.floor(Math.random()*3) + 1) % 3 === 0) ? 'g/' : '') + image.width + '/' + image.height + '/'
-				})));
+			isSingle = ($image.width() === _this.calculatedImageWidth);
 
-				$image.find('a').bind('click', function (evt) {
-					evt.preventDefault();
-
-					alert('Woof!');
-				});
-
-				galleryContainer.append($image);
-			}
-
-			single = ($image.width() === gallery.CALCULATED_IMAGE_WIDTH);
-
-			if ((colCount + ((single) ? 1 : 2)) > colHeights.length) {
+			if ((colCount + ((isSingle) ? 1 : 2)) > colHeights.length) {
 				colCount = 0;
 			}
 
 			$image.css({
-				left		: (colCount * (gallery.CALCULATED_IMAGE_WIDTH + gallery.CONSTANTS.MARGIN)) + 'px',
-				top			: ((single) ? colHeights[colCount] : Math.max(colHeights[colCount], colHeights[colCount + 1])) + 'px',
+				left		: (colCount * (_this.calculatedImageWidth + CONSTANTS.MARGIN)) + 'px',
+				top			: ((isSingle) ? colHeights[colCount] : Math.max(colHeights[colCount], colHeights[colCount + 1])) + 'px',
 				transform	: 'rotate(0)'
 			});
 
-			colHeights[colCount] += ($image.height() + gallery.CONSTANTS.MARGIN);
-			if (!single) {
-				colHeights[colCount + 1] += ($image.height() + gallery.CONSTANTS.MARGIN);
+			colHeights[colCount] += ($image.height() + CONSTANTS.MARGIN);
+			if (!isSingle) {
+				colHeights[colCount + 1] += ($image.height() + CONSTANTS.MARGIN);
 				max = Math.max(colHeights[colCount], colHeights[colCount + 1]);
 
 				colHeights[colCount] = max;
 				colHeights[colCount + 1] = max;	
 			}
 
-			if ((colCount === 0) || ((colHeights[colCount - 1] - colHeights[colCount]) <= gallery.CONSTANTS.GAP_THRESHOLD)) {
-				colCount += ((single) ? 1 : 2);
+			if ((colCount === 0) || ((colHeights[colCount - 1] - colHeights[colCount]) <= CONSTANTS.GAP_THRESHOLD)) {
+				colCount += ((isSingle) ? 1 : 2);
 			}
 		}
-		galleryContainer.height(Math.max.apply(_this, colHeights));
+
+		_this.galleryContainer.height(Math.max.apply(_this, colHeights));
 	}
 
-	this.setupGallery();
+	this.init();
 }
 
 global.handleScrollToTop = function (evt) {
@@ -107,21 +128,20 @@ global.handleScrollToTop = function (evt) {
 }
 
 $(document).ready(function() {
-	gallery = new Gallery(galleryImages);
-
-	$(window).bind('resize', function () {
-		gallery.setupGallery();
-	});
-
 	global.$scrollToTop = $('a#scrollToTop');
 	global.$window = $(window);
 	global.$page = $('body, html');
+
+	gallery = new Gallery(galleryImages);
+	$(window).bind('resize', function () {
+		gallery.init();
+	});
 
 	$(window).bind('scroll', global.handleScrollToTop);
 
 	global.$scrollToTop.bind('click', function (evt) {
 		evt.preventDefault();
-		
+
 		global.$page.animate({
 			scrollTop: 0
 		}, 500);
